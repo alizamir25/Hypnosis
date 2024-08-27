6dd591c7bdb67e68702e8306810a5640a2b82039
alizamir25 <ali0786zamir@gmail.com>
2024-08-28 00:19:59 +0100

    Patch

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var radius = 5;
 var size = 2000;
 var degree = 10;
 var step = degree/size;
-
 document.body.appendChild(canvas);
-
 var c = canvas.getContext('2d');
-
 c.fillStyle = '#fff';
 c.fillRect(0,0,width,height);
-
 c.beginPath();
 c.moveTo(centerX+(i/2), centerY);
 for(var i = 0; i < size; i++)
@@ -27,7 +23,6 @@ for(var i = 0; i < size; i++)
   c.rotate(-degree * (Math.PI / 180));
   c.translate(-centerX, -centerY);
   c.lineTo(centerX+(i/2), centerY); 
-  
 }
   c.lineWidth = 8;
   c.strokeStyle="#000";
